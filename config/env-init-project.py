dd328b3ac860343e40ba1d810005eaa6583a82f4
Shahim Essaid <shahim@essaid.com>
2023-04-27 09:09:38 -0700

    TIMS project settings.

--- config/env-init-project.py
+++ config/env-init-project.py
@@ -9,3 +9,20 @@
 # The full list of environment variables is in config/env-defaults.py.
 
 import os
+
+os.environ['HS_PROFILES'] = os.environ.get('HS_PROFILES', 'env-defaults,hs-defaults,hs-postgresql,hs-elasticsearch,tims,hs-project,hs-local')
+
+# TIMS uses this port offset
+os.environ['HS_PORT_OFFSET'] = os.environ.get('HS_PORT_OFFSET', '10000')
+
+# The following commented out lines can go into local if needed
+# # Set the following to your actual environment.
+# environ['HS_HAPI_HOST'] = '0.0.0.0'
+# environ['HS_HAPI_HOST_PUBLIC'] = '20.3.198.176'
+
+
+os.environ['HS_HAPI_BUILD_GIT_REPO'] = 'https://github.com/HOT-Ecosystem/tims-hapi-build.git'
+os.environ['HS_HAPI_BUILD_GIT_REF'] = 'origin/dev'
+os.environ['HS_HAPI_BUILD_CMD'] = 'mvn -U -Dmaven.test.skip clean package'
+os.environ['HS_HAPI_BUILD_ALWAYS'] = 'true'
+
